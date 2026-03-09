6915fbc09b857af1a3fe154ed455ac532d618569
charvi-web <charvisingh077@gmail.com>
2026-03-09 18:07:53 +0530

    Isomorphic strings

---
+++ isomorphicStrings.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size()!=t.size()) return false;
+        
+        map<char,char>m1;
+        map<char,char>m2;
+        for (int i=0;i<s.size();i++)
+        {
+            if (m1.count(s[i]) && m1[s[i]]!=t[i])
+            {
+                return false;
+            }
+            if (m2.count(t[i]) && m2[t[i]]!=s[i])
+            {
+                return false;
+            }
+            m1[s[i]]=t[i];
+            m2[t[i]]=s[i];
+        }
+        return  true;
+    }
+};
